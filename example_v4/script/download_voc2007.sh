bab651ec83b9be05875e7922a7d3c724f321a1d3
robin <zhangbinatp@gmail.com>
2018-04-28 11:02:52 +0800

    update_some_download_script
    Signed-off-by: robin <zhangbinatp@gmail.com>

---
+++ example_v4/script/download_voc2007.sh
@@ -0,0 +1,13 @@
+#method 1
+wget https://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar
+wget https://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
+wget https://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
+tar xf VOCtrainval_11-May-2012.tar
+tar xf VOCtrainval_06-Nov-2007.tar
+tar xf VOCtest_06-Nov-2007.tar
+
+
+#method 2
+wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
+wget http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtest_06-Nov-2007.tar
